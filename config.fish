564a579b729674561e2804a5369d78dca71e262d
barathrajd <barathdeva757@gmail.com>
2022-05-23 20:59:42 +0530

    fish updated

--- config.fish
+++ config.fish
@@ -15,5 +15,10 @@ zoxide init fish | source
 alias vi=nvim
 alias vim=nvim
 alias lg=lazygit
-# alias ll=exa -I "node_modules|.gitignore" --long --tree --no-time
-# alias ll=exa --long --tree --no-time
+alias ll='exa -I "node_modules|.gitignore" --long --tree --no-time --git --classify'
+alias who-git-config='git config user.name && git config user.email'
+alias affinsys-git-config='git config user.name "barathraj-affinsys" && git config user.email "barathraj@affinsys.com"'
+alias personal-git-config='git config user.name "barathrajd" && git config user.email "barathdeva757@gmail.com"'
+alias cl=clear
+alias cat=batcat
+alias g=git
---
+++ functions/fzf_key_bindings.fish
@@ -0,0 +1,1 @@
+/home/barathraj-affinsys/.fzf/shell/key-bindings.fish
